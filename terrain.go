75db395523d52e55fda2fe33a906ece2a98d28cf
Serdar Kabaoglu <serdarka@gmail.com>
2022-10-03 21:21:00 +0200

    animating background

--- terrain.go
+++ terrain.go
@@ -7,10 +7,10 @@ func (g *Game) drawTerrain() {
 	g.ResetStyle()
 	for x := g.left; x <= g.right; x++ {
 		for y := g.top; y <= g.bottom; y++ {
-			zoom := 16.0
+			zoom := 8.0
 			ambient := 10.0
 			strength := 12.0
-			luminosity := ambient + strength*g.noise.Eval2(float64(x)/zoom, float64(y)/zoom)
+			luminosity := ambient + strength*g.noise.Eval3(float64(x)/zoom, float64(y)/zoom, float64(g.frame)*0.015)
 			g.Background(style.Color(style.Hsl2Rgb(242, 26, luminosity)))
 			g.PutChar(x, y, ' ')
 		}
