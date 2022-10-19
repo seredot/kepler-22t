a42004d6ea9be079201181dfa8acbc6a7e27c73b
Serdar Kabaoglu <serdarka@gmail.com>
2022-10-19 16:13:58 +0200

    fog tuning

--- fog.go
+++ fog.go
@@ -2,17 +2,25 @@ package main
 
 import "github.com/seredot/kepler-22t/color"
 
+// drawFog draws an animating fog effect on the whole screen using Perlin Noise
+// like algorithm. The 2D render actually passes through a 3D noise outputting
+// slices of Z axis over time.
 func (g *Game) drawFog() {
 	g.ResetStyle()
 
 	zoom := 8.0
 	ambient := 0.15
 	strength := 0.2
+	seconds := float64(g.totalT.Seconds())
 
 	for x := 0; x < g.width; x++ {
 		for y := 0; y < g.height; y++ {
-			luminosity := ambient + strength*g.noise.Eval3(float64(x)/zoom, float64(y)/zoom, float64(g.totalT.Seconds()))
-			c := color.Color{R: .6, G: .6, B: 1, A: luminosity}
+			luminosity := ambient + strength*g.noise.Eval3(
+				float64(x)/zoom+seconds*0.4,
+				float64(y)/zoom+seconds*0.4,
+				seconds,
+			)
+			c := color.Color{R: .8, G: .6, B: 1, A: luminosity}
 			g.Background(c)
 			g.Foreground(c)
 
